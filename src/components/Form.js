14d3c817cb38dbd213fa3ef8578c68328ed4629d
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-14 12:26:34 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,13 +7,11 @@ class Form extends React.Component {
         <form>
           <input
             type="text"
-            name="firstName"
             onChange={event => this.props.handleFirstNameChange(event)}
             value={this.props.formData.firstName}
           />
           <input
             type="text"
-            name="lastName"
             onChange={event => this.props.handleLastNameChange(event)}
             value={this.props.formData.lastName}
           />
