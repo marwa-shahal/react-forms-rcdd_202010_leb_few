58ac4b306e0c69992053f8b842d03ad7f954ca2f
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-14 12:27:25 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -22,6 +22,36 @@ class Form extends React.Component {
 }
  
 export default Form;
+//for combining the handle change
+// import React from 'react';
+ 
+// class Form extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <form>
+//           <input
+//             type="text"
+//             name="firstName"
+//             onChange={event => this.props.handleChange(event)}
+//             value={this.props.formData.firstName}
+//           />
+//           <input
+//             type="text"
+//             name="lastName"
+//             onChange={event => this.props.handleChange(event)}
+//             value={this.props.formData.lastName} />
+//         </form>
+//       </div>
+//     )
+//   }
+// }
+ 
+// export default Form;
+
+
+
+
 
 
 
