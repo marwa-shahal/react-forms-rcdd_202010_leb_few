e71db8e5a92d7146ae45392e3f9c7c9bfd77dbab
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-14 12:24:24 +0000

    Automatically backed up by Learn

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -20,6 +20,12 @@ class ParentComponent extends React.Component {
     })
   }
  
+  // handleChange = event => {
+  //   this.setState({
+  //     [event.target.name]: event.target.value
+  //   })
+  // }
+ 
   render() {
     return (
       <Form
